08105c238fa2b62d9cc70250b86743da56159c28
Wengang Zhang <wengang.zh@gmail.com>
2024-08-08 11:54:51 -0400

    Add files via upload
    module 2 notebook and files

---
+++ module2_antibody_design/gen_sequence_for_antibody.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --mem=32g
+#SBATCH --gres=gpu:rtx2080:1
+#SBATCH -c 3
+#SBATCH --output=example_4_non_fixed.out
+
+# source activate mlfold
+
+folder_with_pdbs="./pdb"
+
+output_dir="./output_ab"
+if [ ! -d $output_dir ]
+then
+    mkdir -p $output_dir
+fi
+
+
+path_for_parsed_chains=$output_dir"/parsed_pdbs.jsonl"
+path_for_assigned_chains=$output_dir"/assigned_pdbs.jsonl"
+path_for_fixed_positions=$output_dir"/fixed_pdbs.jsonl"
+chains_to_design="A"
+#The first amino acid in the chain corresponds to 1 and not PDB residues index for now.
+design_only_positions="151 152 153 154 155 156 157 158 159 160 161 162 163 164 165 166 167 168 169 170 171 172 173 174 175 368 369 370 371 372 373 374 375 376 377" #design only these residues; use flag --specify_non_fixed
+
+python ../helper_scripts/parse_multiple_chains.py --input_path=$folder_with_pdbs --output_path=$path_for_parsed_chains
+
+python ../helper_scripts/assign_fixed_chains.py --input_path=$path_for_parsed_chains --output_path=$path_for_assigned_chains --chain_list "$chains_to_design"
+
+python ../helper_scripts/make_fixed_positions_dict.py --input_path=$path_for_parsed_chains --output_path=$path_for_fixed_positions --chain_list "$chains_to_design" --position_list "$design_only_positions" --specify_non_fixed
+
+python ../protein_mpnn_run.py \
+        --jsonl_path $path_for_parsed_chains \
+        --chain_id_jsonl $path_for_assigned_chains \
+        --fixed_positions_jsonl $path_for_fixed_positions \
+        --out_folder $output_dir \
+        --num_seq_per_target 2 \
+        --sampling_temp "0.1" \
+        --seed 37 \
+        --batch_size 1
